0541871517f93c938ffec6a5277a9ef949fd07c1
G K <G K>
2016-05-24 19:39:08 +0300

    the the matrix task

--- max.js
+++ max.js
@@ -1,1 +1,11 @@
-var text ='Hello World!';
+var arr1 = [[1, 1, 1, 1, 1],[1, 1, 1, 1, 1]];
+
+function arrMatrix (arr){
+	for (var i = 0; i < arr.length - 1; i++) {
+		for (var j = 0; j < arr[i].length; j++) {
+			console.log(arr[j]);
+		}
+	}
+}
+
+arrMatrix(arr1);
