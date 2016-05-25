dddc5f43535cbbe0571a33148ddcb787d9b26ba0
G K <G K>
2016-05-25 18:39:05 +0300

    new matrix gen with 3 numb

--- max.js
+++ max.js
@@ -1,29 +1,28 @@
-function arrMatrix (){
-	var arr1 = [];
-	for (var i = 0; i < 2; i++) {
-		arr1[i] = [];
-		for (var j = 0; j < 5; j++) {
-			arr1[i].push(1);
-		}
-	}
-	return arr1;
-}
+
 
 function generateMatrix(){
 	var array = [];
-	for (var i = 0; i < 2; i++) {
+	for (var i = 0; i < 3; i++) {
 		var tmp_array = [];
-		for (var j = 0; j < 5; j++) {
-			tmp_array.push(1);
+		if (i == 0) {
+			for (var j = 0; j < 5; j++) {
+				tmp_array.push(1);
+			}
+		} else if (i == 1) {
+			for (var j = 0; j < 5; j++) {
+				tmp_array.push(2);
+			}
+		} else {
+			for (var j = 0; j < 5; j++) {
+				tmp_array.push(3);
+			}
 		}
+		
 		array.push(tmp_array);
 	}
 	return array;
 }
 
-
-var final_array = arrMatrix();
-console.log(final_array);
 
 var my_array = generateMatrix();
 console.log(my_array);
