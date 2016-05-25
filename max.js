1bac73ec89765393bdf24cbe3ce3ddc3d56ce436
G K <G K>
2016-05-25 22:36:26 +0300

    another answer for matrix

--- max.js
+++ max.js
@@ -4,19 +4,9 @@ function generateMatrix(){
 	var array = [];
 	for (var i = 0; i < 3; i++) {
 		var tmp_array = [];
-		if (i == 0) {
 			for (var j = 0; j < 5; j++) {
-				tmp_array.push(1);
+				tmp_array.push(i + 1);
 			}
-		} else if (i == 1) {
-			for (var j = 0; j < 5; j++) {
-				tmp_array.push(2);
-			}
-		} else {
-			for (var j = 0; j < 5; j++) {
-				tmp_array.push(3);
-			}
-		}
 		
 		array.push(tmp_array);
 	}
