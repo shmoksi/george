f540427837468918e2f06a8a44e2cc038c17ba4f
Oksana Dmytruk <dmytruk.oksi@gmail.com>
2016-05-25 15:22:33 +0300

    fix

--- max.js
+++ max.js
@@ -4,10 +4,26 @@ function arrMatrix (){
 		arr1[i] = [];
 		for (var j = 0; j < 5; j++) {
 			arr1[i].push(1);
-		}		
+		}
 	}
 	return arr1;
 }
 
-console.log(arrMatrix()[0]);
-console.log(arrMatrix()[1]);
+function generateMatrix(){
+	var array = [];
+	for (var i = 0; i < 2; i++) {
+		var tmp_array = [];
+		for (var j = 0; j < 5; j++) {
+			tmp_array.push(1);
+		}
+		array.push(tmp_array);
+	}
+	return array;
+}
+
+
+var final_array = arrMatrix();
+console.log(final_array);
+
+var my_array = generateMatrix();
+console.log(my_array);
