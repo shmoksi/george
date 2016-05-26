d0f5e0bee60c4e98af3b5c02dace61c638955c8b
G K <G K>
2016-05-26 17:12:02 +0300

    the matrix task is done v.2 Shmoksi edition

--- max.js
+++ max.js
@@ -4,12 +4,16 @@ function generateMatrix(){
 	var array = [];
 	for (var i = 0; i < 5; i++) {
 		var tmp_array = [];
-			for (var j = 0; j < 5; j++) {
-				tmp_array.unshift(3);
-				tmp_array[i + 1] = 2;
+		for (var j = 0; j < 5; j++) {
+			
+			if (i == j) {
+				tmp_array[j] = 1;
+			} else if (i > j) {
+				tmp_array[j] = 3;
+			} else {
+				tmp_array[j] = 2;
 			}
-		tmp_array[i] = 1;
-		tmp_array.length = 5;
+		}
 		array.push(tmp_array);
 	}
 	return array;
