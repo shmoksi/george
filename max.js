8cfc2c3261a6c0b877bc938cb2b652d73c218001
G K <G K>
2016-05-26 16:09:31 +0300

    the matrix task is done

--- max.js
+++ max.js
@@ -2,12 +2,14 @@
 
 function generateMatrix(){
 	var array = [];
-	for (var i = 0; i < 3; i++) {
+	for (var i = 0; i < 5; i++) {
 		var tmp_array = [];
 			for (var j = 0; j < 5; j++) {
-				tmp_array.push(i + 1);
+				tmp_array.unshift(3);
+				tmp_array[i + 1] = 2;
 			}
-		
+		tmp_array[i] = 1;
+		tmp_array.length = 5;
 		array.push(tmp_array);
 	}
 	return array;
