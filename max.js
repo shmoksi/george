eba67db4ecaf4ef984bec183a35427ef2bc2874f
G K <G K>
2016-05-27 16:56:20 +0300

    take out on the document the matrix

--- max.js
+++ max.js
@@ -1,31 +1,30 @@
-// function generateMatrix(){
-// 	var array = [];
-// 	for (var i = 0; i < 5; i++) {
-// 		var tmp_array = [];
-// 		for (var j = 0; j < 5; j++) {
+function generateMatrix(){
+	var array = [];
+	for (var i = 0; i < 5; i++) {
+		var tmp_array = [];
+		for (var j = 0; j < 5; j++) {
 
-// 			if (i == j) {
-// 				tmp_array[j] = 1;
-// 			} else if (i > j) {
-// 				tmp_array[j] = 3;
-// 			} else {
-// 				tmp_array[j] = 2;
-// 			}
-// 		}
-// 		array.push(tmp_array);
-// 	}
-// 	return array;
-// }
+			if (i == j) {
+				tmp_array[j] = 1;
+			} else if (i > j) {
+				tmp_array[j] = 3;
+			} else {
+				tmp_array[j] = 2;
+			}
+		}
+		array.push(tmp_array);
+	}
+	return array;
+}
 
-// function drawMatrix() {
-	
-// }
+function drawMatrix(array) {
+	for (var i = 0; i < array.length; i++) {
+		document.write(array[i].join(' ') + '<br>');
+	}
+}
 
-// var div = document.createElement('div');
-// div.innerHTML = "Hello";
-// document.body.appendChild(div);
 
-// var my_array = generateMatrix();
-// drawMatrix(my_array);
 
-document.write("hello");
+var my_array = generateMatrix();
+drawMatrix(my_array);
+
