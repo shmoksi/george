740db36188f943524abeeed00225199f8043a184
G K <G K>
2016-05-27 15:17:31 +0300

    doc.write is disable

--- max.js
+++ max.js
@@ -1,26 +1,31 @@
-function generateMatrix(){
-	var array = [];
-	for (var i = 0; i < 5; i++) {
-		var tmp_array = [];
-		for (var j = 0; j < 5; j++) {
+// function generateMatrix(){
+// 	var array = [];
+// 	for (var i = 0; i < 5; i++) {
+// 		var tmp_array = [];
+// 		for (var j = 0; j < 5; j++) {
 
-			if (i == j) {
-				tmp_array[j] = 1;
-			} else if (i > j) {
-				tmp_array[j] = 3;
-			} else {
-				tmp_array[j] = 2;
-			}
-		}
-		array.push(tmp_array);
-	}
-	return array;
-}
+// 			if (i == j) {
+// 				tmp_array[j] = 1;
+// 			} else if (i > j) {
+// 				tmp_array[j] = 3;
+// 			} else {
+// 				tmp_array[j] = 2;
+// 			}
+// 		}
+// 		array.push(tmp_array);
+// 	}
+// 	return array;
+// }
 
-function drawMatrix(array) {
-	...
-}
+// function drawMatrix() {
+	
+// }
 
+// var div = document.createElement('div');
+// div.innerHTML = "Hello";
+// document.body.appendChild(div);
 
-var my_array = generateMatrix();
-drawMatrix(my_array);
+// var my_array = generateMatrix();
+// drawMatrix(my_array);
+
+document.write("hello");
