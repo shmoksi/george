c0b88d3f6e4edba7bafae5df1d77e13643ab1fd6
G K <G K>
2016-05-31 13:37:04 +0300

    added button for adding new user

--- crud/js/script.js
+++ crud/js/script.js
@@ -14,10 +14,18 @@ var users = [
 ]
 
 $(document).ready(function(){
+	$('.col-1').prepend('<button type="button" class="btn btn-green">Add new user</button>');
+	$('.btn-green').click(function() {
+		console.log('click');
+	});
+
 	$.each(users, function() {
 		$('.table').append('<tr><td>' 
 			+ this.name + '</td><td>' 
 			+ this.age + '</td><td>' 
 			+ this.email + '</td><td><button type="button" class="btn btn-primary">Edit</button><button type="button" class="btn btn-danger">Delete</button></td></tr>');
-	})
-})
+
+	});
+
+});
+
