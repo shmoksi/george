38f02ff00fa52a68e88be95686066e307af1cc36
G K <G K>
2016-05-31 20:31:01 +0300

    added a function adding a new user

--- crud/js/script.js
+++ crud/js/script.js
@@ -14,10 +14,13 @@ var users = [
 ]
 
 $(document).ready(function(){
-	$('.btn-green').on('click', function() {
-		$('#myModal').modal();
-	}); 
 	
+
+
+
+
+
+
 	$.each(users, function() {
 		$('.table').append('<tr><td>'
 			+ this.name + '</td><td>'
@@ -25,5 +28,25 @@ $(document).ready(function(){
 			+ this.email + '</td><td><button type="button" class="btn btn-primary">Edit</button><button type="button" class="btn btn-danger">Delete</button></td></tr>');
 
 	});
+
+
+
+$('.btn-green').on('click', function() {
+		$('#myModal').modal();
+	});
+
+
+	
+
+	$('#add-user').on('click', function() {
+		var name = $('#name').val();
+		var age = $('#age').val();
+		var email = $('#email').val();
+		$('.table').append('<tr><td>' + name + '</td><td>' + age + '</td><td>' + email + '</td></tr>');
+	});
+
+
 });
 
+
+	
